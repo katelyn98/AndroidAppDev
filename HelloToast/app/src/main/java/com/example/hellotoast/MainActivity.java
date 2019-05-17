b90c3cb768f9361e376d987bc1edc03c5e01faa9
Katelyn Morrison <codelikeagirl98@gmail.com>
2019-05-17 12:55:38 -0400

    Adding reset button and view for Nexus 10
    Nexus 10 view in Landscape mode

--- HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -4,18 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+    private int reset_value = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
     public void showWaterfalls(View view) {
+        Toast waterfalls = Toast.makeText(this, R.string.waterfall_message, Toast.LENGTH_SHORT);
+
+        waterfalls.show();
     }
 
     public void countUp(View view) {
+
+        mCount++;
+        if(mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
+
+    }
+
+    public void reset(View view){
+        mShowCount.setText((Integer.toString(reset_value)));
+        mCount = 0;
+
     }
 }
