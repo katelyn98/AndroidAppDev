23d679bb5ba612e331b59d54265a1d774b2cc920
Katelyn Morrison <codelikeagirl98@gmail.com>
2019-05-17 07:53:53 -0400

    adding onClick() to buttons

--- HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showWaterfalls(View view) {
+    }
+
+    public void countUp(View view) {
     }
 }
